ad1cdbfc39b81367c42f8a328ac641236a890731
김인성 <jr435@naver.com>
2020-04-02 07:06:09 +0000

    Revert "Merge branch 'insung-back-user' into 'develop'"
    This reverts merge request !5
    
    Former-commit-id: 8cfe02fb69cd469e3c216923f1f77a6f43481f1f

--- sub2/backend/api/serializers.py
+++ sub2/backend/api/serializers.py
@@ -1,5 +1,6 @@
-from .models import Store, CustomUser, Review, Menu
+from .models import Store
 from rest_framework import serializers
+
 
 class StoreSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,37 +15,4 @@ class StoreSerializer(serializers.ModelSerializer):
             "latitude",
             "longitude",
             "category_list",
-        ]
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = [
-            "id",
-            "gender",
-            "birth_year",
-            "email",
-            "nickname",
-        ]
-
-class ReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = [
-            "id",
-            "store",
-            "user",
-            "total_score",
-            "content",
-            "reg_time",
-        ]
-
-class MenuSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Menu
-        fields = [
-            "id",
-            "store",
-            "menu_name",
-            "price",
         ]
--- sub2/backend/backend/settings.py
+++ sub2/backend/backend/settings.py
@@ -27,35 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-AUTH_USER_MODEL = 'api.CustomUser'
-ACCOUNT_USER_MODEL_USERNAME_FIELD = None
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_UNIQUE_EMAIL = True
-ACCOUNT_USERNAME_REQUIRED = False
-ACCOUNT_AUTHENTICATION_METHOD = 'email'
-ACCOUNT_EMAIL_VERIFICATION = 'none'
-ACCOUNT_CONFIRM_EMAIL_ON_GET = True
-ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = '/?verification=1'
-ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = '/?verification=1'
-
-SITE_ID = 1
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-REST_USE_JWT = True
-
-REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'api.serializers.UserSerializer',
-}
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ),
-}
 
 # Application definition
 
@@ -66,14 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework',
-    'rest_framework.authtoken',
-    'rest_auth',
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'rest_auth.registration',
-    'api',
+    "rest_framework",
+    "api",
 ]
 
 MIDDLEWARE = [
@@ -137,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = "ko"
+LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "Asia/Seoul"
 
