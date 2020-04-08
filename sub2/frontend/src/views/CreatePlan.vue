3ebfc04701d53cb6bf64fb7c593b298041ef96ef
jr31112 <jr435@naver.com>
2020-04-08 18:11:50 +0900

    200408 | S02P22B207-79 | TripPlanner-Map
    Former-commit-id: 6efd73469bed4dd466fafdbcaef26a42d089c0f6

---
+++ sub2/frontend/src/views/CreatePlan.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container class="py-0">
+      <v-alert class="mx-auto my-0 pa-0" type="info" style="text-alig">여행갈 도시를 골라주세요!</v-alert>
+      <v-row>
+          <v-col cols="3" class="py-0">
+              <time-line />
+          </v-col>
+          <v-divider vertical />
+          <v-col cols="8" class="py-0">
+              <select-map />
+          </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+import TimeLine from "../components/createplan/TimeLine"
+import SelectMap from "../components/createplan/SelectMap"
+export default {
+    name:"createplan",
+    components:{
+        SelectMap,
+        TimeLine
+    },
+    date(){
+        return{
+            destinations:[],
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
