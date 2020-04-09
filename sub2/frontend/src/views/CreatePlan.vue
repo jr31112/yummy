af7580b250f484ae08288eb2ed6511a331398248
jr31112 <jr435@naver.com>
2020-04-10 00:31:47 +0900

    200409 | S02P22B207-79 | TripPlanner-AddDate-start/end
    Former-commit-id: 4435da718c9ae317ac7307833960c4b5c696816b

--- sub2/frontend/src/views/CreatePlan.vue
+++ sub2/frontend/src/views/CreatePlan.vue
@@ -2,30 +2,45 @@
   <v-container class="py-0">
       <v-alert class="mx-auto my-0 pa-0" type="info" style="text-alig">여행갈 도시를 골라주세요!</v-alert>
       <v-row>
-          <v-col cols="3" class="py-0">
+          <v-col cols="3" class="py-0" style="bor">
+              <date-plan :datePlan="datePlan" />
               <time-line :destinations="destinations" />
           </v-col>
-          <v-divider vertical />
-          <v-col cols="8" class="py-0">
-              <select-map :destinations="destinations" />
+          <v-col cols="9" class="py-0">
+              <select-map :destinations="destinations" :datePlan="datePlan" />
           </v-col>
       </v-row>
   </v-container>
 </template>
 
 <script>
+import DatePlan from "../components/createplan/DatePlan"
 import TimeLine from "../components/createplan/TimeLine"
 import SelectMap from "../components/createplan/SelectMap"
 export default {
     name:"createplan",
     components:{
         SelectMap,
-        TimeLine
+        TimeLine,
+        DatePlan
     },
     data(){
         return{
             destinations:[],
+            datePlan:{}
         }
+    },
+    methods:{
+        setDatePlan(){
+            var today = new Date().toISOString().substr(0, 10)
+            this.datePlan = {
+                start: today,
+                end:null
+            }
+        },
+    },
+    mounted(){
+        this.setDatePlan()
     }
 }
 </script>
